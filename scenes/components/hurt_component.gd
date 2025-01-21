3581c1c45bfdeda8489d8ff1a27b6a2e3dcb349f
Daniel Dang <dnld1ng@yahoo.com>
2025-01-21 17:11:34 -0500

    Fixed current tool bug and bug where all trees shake

--- scenes/components/hurt_component.gd
+++ scenes/components/hurt_component.gd
@@ -7,6 +7,6 @@ signal hurt
 
 func _on_area_entered(area: Area2D) -> void:
 	var hit_component : HitComponent = area as HitComponent
-	
+
 	if tool == hit_component.current_tool:
 		hurt.emit(hit_component.hit_damage) 
