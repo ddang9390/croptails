217c3d7781cbc08155da0b9a0100b055efc0b20c
Daniel Dang <dnld1ng@yahoo.com>
2025-01-24 18:52:36 -0500

    Made rock and stone scenes

---
+++ scenes/objects/rocks/rock.gd
@@ -0,0 +1,23 @@
+extends Sprite2D
+
+@onready var hurt_component: HurtComponent = $HurtComponent
+@onready var damage_component: DamageComponent = $DamageComponent
+
+var stone_scene : PackedScene = preload("res://scenes/objects/rocks/stone.tscn")
+
+func _ready() -> void:
+	hurt_component.hurt.connect(on_hurt)
+	damage_component.max_damage_reached.connect(on_max_damage_reached)
+	
+func on_hurt(hit_damage : int) -> void:
+	damage_component.apply_damage(hit_damage)
+
+	
+func on_max_damage_reached() -> void:
+	call_deferred("add_stone_scene")
+	queue_free()
+
+func add_stone_scene() -> void:
+	var stone_instance : Node2D = stone_scene.instantiate() as Node2D
+	stone_instance.position = global_position
+	get_parent().add_child(stone_instance)
